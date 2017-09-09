898b984a85123d3d7d9a9603f8c83067650cabc4
Rohan Banerjee <banerjee.rohan98@gmail.com>
2017-09-10 02:58:31 +0530

    Update loginWin.py

--- loginWin.py
+++ loginWin.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 import time
-chromedriver = "./chromedriver.exe"
+chromedriver = "./chromedriver.exe"  #download the chrome web driver for the automation
 
 browser = webdriver.Chrome(chromedriver)
-browser.get('https://192.168.10.3/connect/PortalMain')
+browser.get('https://192.168.10.3/connect/PortalMain')  #directs to the webpage
 time.sleep(2)
-username = browser.find_element_by_id("LoginUserPassword_auth_username")
-username.send_keys("RA1611003010758")
+username = browser.find_element_by_id("LoginUserPassword_auth_username")  #goes to the mentioned field
+username.send_keys("Login")  #enter you login id
 
-password = browser.find_element_by_id("LoginUserPassword_auth_password")
-password.send_keys("Cse^3980")
+password = browser.find_element_by_id("LoginUserPassword_auth_password")  #goes to the mentioned field
+password.send_keys("Password")  #enter your password
 
-browser.execute_script("oAuthentication.submitActiveForm()")
+browser.execute_script("oAuthentication.submitActiveForm()")  #clicks the login button
